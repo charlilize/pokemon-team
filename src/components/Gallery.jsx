513905fc7b087334d078d87303ba865ca71cce09
Charlize <148724296+charlilize@users.noreply.github.com>
2024-04-06 23:11:51 -0700

    Pokemon Gallery displays pokemon from database

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,38 @@
 import React, { useState, useEffect } from 'react';
-
 import { supabase } from "../client";
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card";
+import { Button } from "@/components/ui/button"; 
 
 const Gallery = () => {
-
     const [pokemon, setPokemon] = useState([]);
+
+    const typeToImageMap = {
+        Bug: "src/assets/pokemon-types/bug.png",
+        Dark: "src/assets/pokemon-types/dark.png",
+        Dragon: "src/assets/pokemon-types/dragon.png",
+        Electric: "src/assets/pokemon-types/electric.png",
+        Fairy: "src/assets/pokemon-types/fairy.png",
+        Fighting: "src/assets/pokemon-types/fighting.png",
+        Fire: "src/assets/pokemon-types/fire.png",
+        Flying: "src/assets/pokemon-types/flying.png",
+        Ghost: "src/assets/pokemon-types/ghost.png",
+        Grass: "src/assets/pokemon-types/grass.png",
+        Ground: "src/assets/pokemon-types/ground.png",
+        Ice: "src/assets/pokemon-types/ice.png",
+        Normal: "src/assets/pokemon-types/normal.png",
+        Poison: "src/assets/pokemon-types/poison.png",
+        Psychic: "src/assets/pokemon-types/psychic.png",
+        Rock: "src/assets/pokemon-types/rock.png",
+        Steel: "src/assets/pokemon-types/steel.png",
+        Water: "src/assets/pokemon-types/water.png",
+      };
 
     useEffect(() => {
         const fetchPokemon = async () => {
@@ -18,16 +46,32 @@ const Gallery = () => {
         fetchPokemon(); // Call the function within the useEffect hook
       }, []); // Empty dependency array to run the effect only once on component mount
     
-    return (
-        <div>
-            {
-                pokemon && pokemon.length > 0 ?
-                pokemon.map((pokemon, index) => 
-                   <Card key={pokemon.id} id={pokemon.id} name={pokemon.name} type={pokemon.type} hp={pokemon.hp}/>
-                ) : <h2>{'No Pokemon Yet 🥲'}</h2>
-            }
-        </div>  
-    )
+      return (
+        <>
+            <h1 className="font-bold text-3xl my-10">Your Pokemon Team!</h1>
+            <div className="flex flex-wrap gap-10">
+            {pokemon && pokemon.length > 0 ? (
+                pokemon.map((pokemon) => (
+                <Card key={pokemon.id} id={pokemon.id}>
+                    <CardHeader>
+                    <CardTitle>{pokemon.name}</CardTitle>
+                        <img src={typeToImageMap[pokemon.type]} />
+                    </CardHeader>
+                    <CardContent>
+                    <p className="text-xl">Type: {pokemon.type}</p>
+                    <p className="text-xl">HP: {pokemon.hp}</p>
+                    </CardContent>
+                    <CardFooter className="flex justify-center">
+                        <Button>Edit</Button>
+                    </CardFooter>
+                </Card>
+                ))
+            ) : (
+                <h2>{"No Pokemon's created yet 😞"}</h2>
+            )}
+            </div>
+        </>
+      );
 }
 
 export default Gallery;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./routes/Layout";
 import CreatePokemon from './components/CreatePokemon.jsx';
+import Gallery from "./components/Gallery.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
@@ -12,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Layout/>}>
           <Route index={true} path="/" element={<App/>} />
           <Route index={true} path="/create" element={<CreatePokemon />} />
-          <Route index={true} path="/gallery" element={<App />} />
+          <Route index={true} path="/gallery" element={<Gallery/>} />
         </Route> 
     </Routes>
   </BrowserRouter>
